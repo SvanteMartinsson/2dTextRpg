7b501b8651082591520a51b25b41b1c98bb5875a
Svante Martinsson <svantemartinsson@Svantes-Air.lan>
2016-09-27 21:41:24 +0200

    Added a class

---
+++ Uppgifter/src/test.java
@@ -0,0 +1,26 @@
+
+public class test {
+	
+	
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		
+		x = 10;
+		
+		y = 25;
+		
+		System.out.println("Hello world");
+		
+		if(x == 5){
+			System.out.println("Test");
+		}
+		
+		if(y == 25){
+			System.out.println("Good");
+		}
+
+	}
+
+}
