2c83cf6745dd44e1fc73e5605397720f06db86d3
Svante Martinsson <svantemartinsson@Svantes-Air.lan>
2016-09-27 21:44:05 +0200

    Changed code in test.java file

--- Uppgifter/src/test.java
+++ Uppgifter/src/test.java
@@ -4,17 +4,15 @@ public class test {
 	
 
 	public static void main(String[] args) {
-		int x;
-		int y;
+		int x = 10;
+		int y = 25;
 		
-		x = 10;
 		
-		y = 25;
 		
 		System.out.println("Hello world");
 		
-		if(x == 5){
-			System.out.println("Test");
+		if(x<5){
+			System.out.println("test");
 		}
 		
 		if(y == 25){
